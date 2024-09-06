d1e819d0c9f57c95922671b6fd5b1369435b8291
2867744246 <2867744246@qq.com>
2024-09-07 00:59:40 +0800

    违约认定审核功能完成

--- src/stores/modules/user.js
+++ src/stores/modules/user.js
@@ -3,36 +3,49 @@ import { ref } from 'vue'
 import { userGetInfoService } from '../../api/user'
 
 // 用户模块 token setToken removeToken
-export const useUserStore = defineStore(
-  'big-user',
-  () => {
-    const token = ref('')
-    const setToken = (newToken) => {
-      token.value = newToken
-    }
-    const removeToken = () => {
-      token.value = ''
-    }
-
-    const user = ref({})
-    const getUser = async () => {
-      const res = await userGetInfoService() // 请求获取数据
-      user.value = res.data.data
-    }
-    const setUser = (obj) => {
-      user.value = obj
-    }
-
-    return {
-      token,
-      setToken,
-      removeToken,
-      user,
-      getUser,
-      setUser
-    }
-  },
-  {
-    persist: true
+export const useUserStore = defineStore('big-user', () => {
+  const token = ref('')
+  const setToken = (newToken) => {
+    token.value = newToken
   }
-)
+  const removeToken = () => {
+    token.value = ''
+  }
+
+  const user = ref({
+    userID: null, // 添加 userID 属性
+    username: '',
+    nickname: '',
+    user_pic: ''
+  })
+
+  const getUser = async () => {
+    const res = await userGetInfoService() // 请求获取数据
+    user.value = {
+      userID: res.data.data.id, // 设置 userID
+      username: res.data.data.username,
+      nickname: res.data.data.nickname,
+      user_pic: res.data.data.user_pic
+    }
+  }
+
+  const setUser = (obj) => {
+    user.value = {
+      userID: obj.id, // 设置 userID
+      username: obj.username,
+      nickname: obj.nickname,
+      user_pic: obj.user_pic
+    }
+  }
+
+  return {
+    token,
+    setToken,
+    removeToken,
+    user,
+    getUser,
+    setUser
+  }
+}, {
+  persist: true
+})
