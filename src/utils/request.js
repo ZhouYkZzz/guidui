a7f6cef68a3ed96ad17b8243b90cb0d700f2413b
2867744246 <2867744246@qq.com>
2024-09-07 22:37:38 +0800

    123

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 //import { useUserStore } from '@/stores'
 import { ElMessage } from 'element-plus'
 import router from '@/router'
-const baseURL = 'https://280a-2409-8929-b46c-84dc-ffa1-a777-ce3f-3e60.ngrok-free.app'
+const baseURL = 'https://192.168.31.39:8080'
 
 const instance = axios.create({
   // TODO 1. 基础地址，超时时间
